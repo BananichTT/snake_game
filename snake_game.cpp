b10396ef20c98e0bcb0efa81bd59fdd64937b671
Buldin Alexandr <buldin.alieksandr@gmail.com>
2024-06-18 18:40:54 +0300

    Добавил генерацию фруктов и их поедание змейкой

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,6 +1,7 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <vector>
-
+#include <cstdlib>
+#include <ctime>
 
 
 struct SnakeSegment {
@@ -13,11 +14,17 @@ int main() {
 
     std::vector<SnakeSegment> snake;
     snake.push_back(SnakeSegment(5, 5));  // Начальная позиция змейки
+
     const int blockSize = 20;  // Размер блока змейки
     int dx = 1, dy = 0;  // Направление движения
 
     sf::Clock clock;
     float timer = 0, delay = 0.1;
+
+    // Генерация фрукта
+    srand(time(0));
+    int fruitX = rand() % (800 / blockSize);
+    int fruitY = rand() % (600 / blockSize);
 
     while (window.isOpen()) {
         float time = clock.restart().asSeconds();
@@ -43,16 +50,30 @@ int main() {
             }
             snake[0].x += dx;
             snake[0].y += dy;
+
+            // Проверка на поедание фрукта
+            if (snake[0].x == fruitX && snake[0].y == fruitY) {
+                snake.push_back(SnakeSegment(fruitX, fruitY));
+                fruitX = rand() % (800 / blockSize);
+                fruitY = rand() % (600 / blockSize);
+            }
         }
 
         window.clear();
 
+        // Отрисовка змейки
         for (auto& segment : snake) {
             sf::RectangleShape rectangle(sf::Vector2f(blockSize, blockSize));
             rectangle.setFillColor(sf::Color::Green);
             rectangle.setPosition(segment.x * blockSize, segment.y * blockSize);
             window.draw(rectangle);
         }
+
+        // Отрисовка фрукта
+        sf::RectangleShape fruit(sf::Vector2f(blockSize, blockSize));
+        fruit.setFillColor(sf::Color::Red);
+        fruit.setPosition(fruitX * blockSize, fruitY * blockSize);
+        window.draw(fruit);
 
         window.display();
     }
